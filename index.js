9cac0b03a4541445cba9a98f8486ea46e044b7f0
Leonel Velez <leonel-14-velez@hotmail.com>
2021-08-28 19:45:00 -0500

    agregando spinner

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 const onConsultarApi = async ()=>{
     try
     {
-
+        const input = document.getElementById("url")    
+        let spinner = document.getElementById("spinner")
+        spinner.style.display = "flex"
+        const rawResponse = await fetch('https://rim-ug-api.azurewebsites.net/predecir', {
+            method: 'POST',
+            headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({imagen: input.value})
+        });
+        const content = await rawResponse.json()
+        console.log(content)
+        if(rawResponse.ok)
+        {
+            if(!content.error)
+            {
+                document.getElementById("madurez").innerHTML = content.respuesta
+            }
+            
+        }        
+        spinner.style.display = "none"
     }catch(error){
-        console.log("")
+        spinner.style.display = "none"
+        console.log(error)
     }
-    const input = document.getElementById("url")    
-    const rawResponse = await fetch('https://rim-ug-api.azurewebsites.net/predecir', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({imagen: input.value})
-      });
-    const content = await rawResponse.json()
-    console.log(content)
-    document.getElementById("madurez").innerHTML = content.respuesta
+    
     
 }
+
